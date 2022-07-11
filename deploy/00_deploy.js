dcc845d6c63046221aa087543b318d17f2a208f7
Alex Aunter <lxaunter@gmail.com>
2022-07-11 22:51:29 +0400

    style: fix config

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -1,10 +1,10 @@
 // deploy/00_deploy_my_contract.js
-module.exports = async ({getNamedAccounts, deployments}) => {
-  const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
   await deploy('NFTG0RARE', {
     from: deployer,
-    args: ["NFTG0RARE", "RARES", 100, 10000, 200],
+    args: ['NFTG0RARE', 'RARES', 100, 10000, 200],
     log: true,
   });
 };
